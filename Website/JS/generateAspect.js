898878598b4be90f4d65e359e318fda180790290
Sem Pruijs <sem@pruijs.nl>
2022-09-03 17:35:17 +0200

    Make jsonToObject function

--- Website/JS/generateAspect.js
+++ Website/JS/generateAspect.js
@@ -1,35 +1,13 @@
 const aspectUrl = "Content/KenmerkendeAspecten.json"
 
+// Gets json file, returns javascript object
+function jsonToObject(jsonString) {
+    return JSON.parse(jsonString);    
+}
+
 function GenerateAspect(jsonString) {
-    // clearProjectsContainer();
+    let aspectContent = jsonToObject(jsonString);
 
-    // let input = document.getElementById("SearchBar").value;
-
-    let aspectContent = JSON.parse(jsonString);
-    // let projectsContainer = document.getElementById("ProjectsContainer");
-    // aspectContent.Projects.forEach(Project => {
-    //     var inputContainsTag = false
-    //     Project.Tags.toUpperCase().split(", ").forEach(tag => {
-    //         if (tag.includes(input.toUpperCase())) {
-    //             inputContainsTag = true
-    //         }
-    //     })
-    //     if (Project.Title.toUpperCase().includes(input.toUpperCase()) || input === "" || inputContainsTag) {
-    //         console.log(input)
-    //         //Make project section
-    //         let projectSection = document.createElement("section");
-    //         projectsContainer.appendChild(projectSection);
-
-    //         //Generate title and content
-    //         GenerateElement("h3", "projects__title", Project.Title, projectSection);
-    //         GenerateElement("div", "projects__content", Project.Content, projectSection);
-
-    //         //Generate buttons
-    //         Project.Buttons.forEach(Button => {
-    //             GenerateButton("a", Button.ButtonClass, Button.ButtonText, Button.ButtonHref, projectSection);
-    //         })
-    //     }
-    // })
     console.log(aspectContent.chapters[0].title);
     console.log(aspectContent.chapters[0].aspects);
     console.log(aspectContent.chapters[0].aspects[0].id);
