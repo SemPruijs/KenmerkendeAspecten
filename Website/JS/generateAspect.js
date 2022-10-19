0f1938a11b60ccfce583d130ba43e3aa878d3517
Sem Pruijs <sem@pruijs.nl>
2022-10-19 16:47:12 +0200

    Make selectedAspect

--- Website/JS/generateAspect.js
+++ Website/JS/generateAspect.js
@@ -1,5 +1,9 @@
 const aspectUrl = "Content/KenmerkendeAspecten.json"
 
+function SelectedAspect() {
+    return 1;
+}
+
 function GenerateAspect(aspectContent) {
 
     console.log(aspectContent.chapters[0].title);
@@ -7,7 +11,7 @@ function GenerateAspect(aspectContent) {
     console.log(aspectContent.chapters[0].aspects[0].id);
     console.log(aspectContent.chapters[0].aspects[0].aspect);
 
-    document.getElementById("question").innerHTML = aspectContent.chapters[0].aspects[0].aspect;
+    document.getElementById("question").innerHTML = aspectContent.chapters[0].aspects[SelectedAspect()].aspect;
     
     // console.log(aspectContent.chapters[0][0].aspect);
 }
