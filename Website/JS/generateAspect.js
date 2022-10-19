d976f7bcc11af17cd694943a238706f645e800ad
Sem Pruijs <sem@pruijs.nl>
2022-10-19 20:50:15 +0200

    Better function names and functionality

--- Website/JS/generateAspect.js
+++ Website/JS/generateAspect.js
@@ -1,4 +1,5 @@
 const aspectUrl = "Content/KenmerkendeAspecten.json"
+const asyncAspects = GenerateAspects()
 
 function answerTextfieldOnEnter(event) {
     if (event.key == "Enter") {
@@ -7,32 +8,45 @@ function answerTextfieldOnEnter(event) {
     }
 }
 
+function randomIndexFor(list) {
+    return Math.floor(Math.random() * list.length);
+}
+
 function SelectedAspect() {
     return 1;
 }
 
-function GenerateAspect(aspectContent) {
+// function GenerateAspect(aspectContent) {
 
-    console.log(aspectContent.chapters[0].title);
-    console.log(aspectContent.chapters[0].aspects);
-    console.log(aspectContent.chapters[0].aspects[0].id);
-    console.log(aspectContent.chapters[0].aspects[0].aspect);
+//     console.log(aspectContent.chapters[0].title);
+//     console.log(aspectContent.chapters[0].asyncAspects);
+//     console.log(aspectContent.chapters[0].asyncAspects[0].id);
+//     console.log(aspectContent.chapters[0].asyncAspects[0].aspect);
 
-    document.getElementById("question").innerHTML = aspectContent.chapters[0].aspects[SelectedAspect()].aspect;
+//     document.getElementById("question").innerHTML = aspectContent.chapters[0].asyncAspects[SelectedAspect()].aspect;
     
-    // console.log(aspectContent.chapters[0][0].aspect);
-}
+//     // console.log(aspectContent.chapters[0][0].aspect);
+// }
 
 
 
-async function renderAspect() {
+async function GenerateAspects() {
     const res = await fetch(aspectUrl);
     const body = await res.json();
 
     console.log(body);
 
-    GenerateAspect(body);
+    return body;
 }
 
-renderAspect();
+async function renderAspect() {
+    var aspects = await asyncAspects
+    document.getElementById("question").innerHTML = aspects.chapters[0].aspects[SelectedAspect()].aspect 
+    // console.log("test)
+    // console.log(aspects
+}
+// 
+renderAspect()
+
+// GenerateAspects();
 
