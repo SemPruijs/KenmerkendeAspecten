4e5bdcbee0f2aee183b1caeb84d5ee9fc720efeb
Sem Pruijs <sem@pruijs.nl>
2022-10-22 18:20:12 +0200

    Make GetAspect function

--- Website/JS/generateAspect.js
+++ Website/JS/generateAspect.js
@@ -1,5 +1,6 @@
 const aspectUrl = "Content/KenmerkendeAspecten.json"
-const asyncAspects = GenerateAspects()
+const asyncAspects = GetAspects()
+
 
 function answerTextfieldOnEnter(event) {
     if (event.key == "Enter") {
@@ -10,6 +11,12 @@ function answerTextfieldOnEnter(event) {
 
 function randomIndexFor(list) {
     return Math.floor(Math.random() * list.length);
+}
+
+// TODO: Call this function on a chapter
+async function randomIndexFromChapter(chapter) {
+    var aspects = await asyncAspects
+    return randomIndexFromChapter(aspects.chapters[chapter])
 }
 
 function SelectedAspect() {
@@ -30,7 +37,7 @@ function SelectedAspect() {
 
 
 
-async function GenerateAspects() {
+async function GetAspects() {
     const res = await fetch(aspectUrl);
     const body = await res.json();
 
@@ -39,14 +46,18 @@ async function GenerateAspects() {
     return body;
 }
 
+function GetAspect(aspects, chapter, index) {
+    return aspects.chapters[chapter].aspects[index]
+}
+
+
+
 async function renderAspect() {
     var aspects = await asyncAspects
-    document.getElementById("question").innerHTML = aspects.chapters[0].aspects[SelectedAspect()].aspect 
-    // console.log("test)
-    // console.log(aspects
+    document.getElementById("question").innerHTML = GetAspect(aspects, 0, 0).aspect
 }
-// 
+
 renderAspect()
 
-// GenerateAspects();
+
 
