d8f5f6fd36d25ee67a6b4d435a0cc6c5d92cee60
Sem Pruijs <sem@pruijs.net>
2022-11-16 00:19:41 +0100

    Change discription of flake

--- Website/flake.nix
+++ Website/flake.nix
@@ -1,5 +1,8 @@
 {
-  description = "A very basic flake";
+  description = "Built system for website";
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/release-22.05";
+  };
 
   outputs = { self, nixpkgs }: {
 
