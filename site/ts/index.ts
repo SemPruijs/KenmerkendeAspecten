e0f4b5356783cf36a30532f346acb06a81305bdc
Sem Pruijs <sem@pruijs.net>
2022-11-27 00:17:19 +0100

    Add commends to the newOlder function

--- site/ts/index.ts
+++ site/ts/index.ts
@@ -84,12 +84,17 @@ function listAspectsFromChapters(chapters: Array<Chapter>): Array<Aspect> {
 
 // The last 20% of the aspects will appear in the last 80% of the new older.
 function generateNewOrder(oldOrder: Array<Aspect>): Array<Aspect> {
+    // make 2 arrays, the firstSplit is the first 80%, last split the remaining 20%
     const firstSplit = oldOrder.slice(0, Math.floor(oldOrder.length * 0.8))
     const lastSplit = oldOrder.slice(Math.floor(oldOrder.length * 0.8), oldOrder.length)
 
+    // shuffle the first 80%
     const shuffledFirstSplit = shuffle(firstSplit)
+
+    // remove the first 20% of the shuffled 80%. These percentages are based on the whole list length.
     const noStartShuffleFirstSplit = shuffledFirstSplit.slice(lastSplit.length, shuffledFirstSplit.length)
 
+    // make list based on the list above and append the aspects from the last 20% of the list (The second list in this function)
     let NSSFSWithLastPart = noStartShuffleFirstSplit 
 
     for (let i = 0; i < lastSplit.length; i++) {
@@ -97,7 +102,10 @@ function generateNewOrder(oldOrder: Array<Aspect>): Array<Aspect> {
         NSSFSWithLastPart.splice(randomIndex, 0, lastSplit[i])
     }
 
+    // get the starting aspects of the shuffled first 80# that where not include in the new last 80%
     const start = shuffledFirstSplit.slice(0, lastSplit.length)
+
+    // create the newOlder list, with the new 20% and the new 80$
     const newOrder = start.concat(NSSFSWithLastPart)
 
     return newOrder
